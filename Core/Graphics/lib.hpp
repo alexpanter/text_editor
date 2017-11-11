89115bc9eb1784cc392a5083e795429c4d9e133f
Alexander Christensen <alex_c007@hotmail.com>
2017-11-11 18:38:52 +0100

    GUI: defining basic structures for the API

--- Core/Graphics/lib.hpp
+++ Core/Graphics/lib.hpp
@@ -9,10 +9,12 @@
 
 #include <vector>
 
-// TODO: apply strategy pattern here for graphics interface
-
+// TODO: should screen coordinates be used in normalized coordinates instead of pixels?
+typedef coord_t unsigned int;
 
 typedef enum {
+    Fill,
+
     OnlyLeft,
     OnlyRight,
     OnlyTop,
@@ -33,44 +35,51 @@ typedef enum {
     OnlyTopAndBottom
 } DockStyle;
 
-class GUIControlBlock { // TODO: devise a better name.
+class IMouseEventControl {
+public:
+    virtual void OnClick(coord_t x, coord_t y) = 0;
+    virtual void OnMouseHover() = 0;
+    virtual void OnMouseEnter() = 0;
+    virtual void OnMouseLeave() = 0;
+};
+class IKeyboardEventControl {
+public:
+    virtual void OnKeyPress() = 0; // TODO: use template for variadic argument list!
+    virtual void OnKeyRelease() = 0;
+}
+
+class IGUIControlBlock { // TODO: devise a better name.
 public:
     // TODO: define common properties for window, panels, text boxes, buttons, etc.
-    GUIBlock* _parent;
-    std::vector <GUIBlock*> _children;
+    GUIControlBlock* _parent;
+    std::vector <GUIControlBlock*> _children;
+    // there must always be a docking style for any GUI control block
     DockStyle _docking; // TODO: dock or docking?
 };
 
-class WindowPosition {
-public:
-    // in screen pixels
-    unsigned int xLocation, yLocation;
-    unsigned int width, height;
-};
 class IWindow {
 protected:
-    WindowPosition _info; // TODO: is info a proper name?
+    unsigned int width, height;
+    unsigned int xLocation, yLocation;
+    GUIControlBlock _main_panel; // there must always be a main panel attached!
 public:
-    virtual void OnResize(WindowPosition info) = 0; // TODO: marked for deletion??
+    virtual void OnResize(coord_t width, coord_t height) = 0;
+    virtual void OnMove(coord_t x, coord_t y) = 0; // pixel coordinates
     virtual void OnClose() = 0;
     virtual void OnMaximize() = 0;
     virtual void OnMinimize() = 0;
 };
 
-
-
 class IPanel {
 protected:
     IWindow* _window; // TODO: do we need this reference?
     GUIControlBlock _control;
-public:
-    virtual
 };
 class ITextBox {
-public:
-
+protected:
+    GUIControlBlock _control;
 };
 class IButton {
-public:
-
+protected:
+    GUIControlBlock _control;
 };
