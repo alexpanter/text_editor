9f8e5bb9329e6602c477c114802997bf0fb0e57d
Alexander Christensen <alex_c007@hotmail.com>
2018-03-11 17:31:59 +0100

    Add main script and Makefile as application placeholder

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include "context/applicationContext.hpp"
+
+int main()
+{
+    ApplicationContext* application; // TODO: use a pointer instead?
+
+    while(application->IsRunning()) {
+        // TODO: do some things here!
+    }
+
+    return 0;
+}
